3ca61533e512e47fbb16d7325c5e6ae5fdf4e327
VoodooMood <trufanov.ivannn@yandex.ru>
2026-01-07 00:06:54 +0300

    неведомая хрень повтор

---
+++ хуйня в пакете/хуйня в пакете/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace хуйня_в_пакете
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblHitPoints = new System.Windows.Forms.Label();
+            this.lblBags = new System.Windows.Forms.Label();
+            this.lblLevel = new System.Windows.Forms.Label();
+            this.lblfoodwaseaten = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Здоровье";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пакетики";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Сожрано корма";
+            // 
+            // lblHitPoints
+            // 
+            this.lblHitPoints.AutoSize = true;
+            this.lblHitPoints.Location = new System.Drawing.Point(110, 19);
+            this.lblHitPoints.Name = "lblHitPoints";
+            this.lblHitPoints.Size = new System.Drawing.Size(35, 13);
+            this.lblHitPoints.TabIndex = 3;
+            this.lblHitPoints.Text = "label4";
+            // 
+            // lblBags
+            // 
+            this.lblBags.AutoSize = true;
+            this.lblBags.Location = new System.Drawing.Point(110, 45);
+            this.lblBags.Name = "lblBags";
+            this.lblBags.Size = new System.Drawing.Size(35, 13);
+            this.lblBags.TabIndex = 4;
+            this.lblBags.Text = "label5";
+            // 
+            // lblLevel
+            // 
+            this.lblLevel.AutoSize = true;
+            this.lblLevel.Location = new System.Drawing.Point(110, 100);
+            this.lblLevel.Name = "lblLevel";
+            this.lblLevel.Size = new System.Drawing.Size(35, 13);
+            this.lblLevel.TabIndex = 5;
+            this.lblLevel.Text = "label6";
+            // 
+            // lblfoodwaseaten
+            // 
+            this.lblfoodwaseaten.AutoSize = true;
+            this.lblfoodwaseaten.Location = new System.Drawing.Point(110, 73);
+            this.lblfoodwaseaten.Name = "lblfoodwaseaten";
+            this.lblfoodwaseaten.Size = new System.Drawing.Size(35, 13);
+            this.lblfoodwaseaten.TabIndex = 6;
+            this.lblfoodwaseaten.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(18, 100);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(51, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Уровень";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(719, 651);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblfoodwaseaten);
+            this.Controls.Add(this.lblLevel);
+            this.Controls.Add(this.lblBags);
+            this.Controls.Add(this.lblHitPoints);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Игра Кокоса";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblHitPoints;
+        private System.Windows.Forms.Label lblBags;
+        private System.Windows.Forms.Label lblLevel;
+        private System.Windows.Forms.Label lblfoodwaseaten;
+        private System.Windows.Forms.Label label8;
+    }
+}
+

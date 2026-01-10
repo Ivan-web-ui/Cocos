83489a6ac3d33f07f54d2d7314bf6f7937b5c779
VoodooMood <trufanov.ivannn@yandex.ru>
2026-01-11 00:07:15 +0300

    пошла какая то хуйня

--- хуйня в пакете/хуйня в пакете/Form1.cs
+++ хуйня в пакете/хуйня в пакете/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClassLibrary1;
 
 namespace хуйня_в_пакете
 {
     public partial class Form1 : Form
     {
+        private Player _player;
         public Form1()
+
         {
             InitializeComponent();
+            _player = new Player(20,20,10,0,1);
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblBags.Text = _player.Bags.ToString();
+            lblfoodWaseaten.Text = _player.FoodWaseaten.ToString();
+            lblLevel.Text = _player.Level.ToString();
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
